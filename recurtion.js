b7590bb5052d54ed3523497c5d1107cc453aa046
osiris <kontchou05@gmail.com>
2021-12-14 20:22:09 +0100

    v2

--- recurtion.js
+++ recurtion.js
@@ -1,19 +1,33 @@
-const object = [1,2,3]
-n = object.length
-console.log(recurtion(object,n))
+/*const object = ["jay","max","jack"]
 
  function recurtion(object, n){
-     let i
-     if(i==n){
-         i = 0
-         return 
+     if(n<0){
+         return n
      }else{
-         console.log(object[i])
-         i++
-          recurtion(object, n)
+         console.log(object[n])
+       
+        return recurtion(object, n-1)
 
 
      }
  }
 
- 
+ console.log(recurtion(object,3))
+hasProperty("location", {"location": {"longitude": 1.2345, "latitude": 5.4321}})
+
+ //******************************** */
+
+ hasProperty("location", {"location": {"longitude": 1.2345, "latitude": 5.4321}})
+ var replaceKeysInObj = (property,object) => {
+    const newObj = {};
+    for (const key in obj){
+      if(ogKey === key) {
+        newObj[newKey] = obj[ogKey]
+   } else if (typeof obj[ogKey] === 'object') {
+     newObj[ogKey] = replaceKeysInObj(obj[ogKey], key, newKey);
+   } else {
+     newObj[ogKey] = obj[ogKey]
+   }
+ }
+ return newObj
+};
--- test.js
+++ test.js
@@ -1,42 +1,31 @@
-const object = {"location": {"longitude": 1.2345, "latitude": 5.4321}}
-const property = "location"
-
 const hasProperty = (property, object)=>{
 
-// trueOrFalse
-const trueOrFalse = {t_True : true, t_False : false}
-//should return false when the property is not a string
-
-if(typeof(property) !== "string"){
-  console.log(trueOrFalse.t_False+" test 1")
-  return trueOrFalse.t_False
-}
-//should return false when the object is not an object
-if(typeof(property) !== "object"){
-    console.log(trueOrFalse.t_False+" test 2")
-    return trueOrFalse.t_False
-}
-
-for(const key in object){
-       //) should return true when a property is inside an object
-        if(property === object[key]){
-          console.log(trueOrFalse.t_True+" test 3")
-          return trueOrFalse.t_True
+        if(typeof property === 'string' ){
+            if(typeof object === 'object'){
+              console.log("True for 2 and 3")
+                for(prop in object){
+                  if(property === prop){
+                    return true
+                  }else{
+                        if(typeof object[prop] === 'object'){
+                          return hasProperty(property, object[prop]) 
+                      }else{
+                          return false;
+                      }
+                  }
+              }
+            }else{
+              console.log("test 3")
+              return false;
+            }
+            
         }else{
-          //should return false when a property is not inside an object
-          console.log(trueOrFalse.t_False+" test 5")
-        
-        }
-       //should return true when a property is really deeply nested inside an object
-        if(property === object[key][property]){
-          console.log(trueOrFalse.t_True+" test 4")
-          return trueOrFalse.t_False
-        }else{
-          // should return false when a property is not deeply nested inside an object
-          console.log(trueOrFalse.t_False+" test 6")
-        }
-    }
+            console.log("test 2")
+            return false;
+        } 
+  
 }
 
 
-console.log(hasProperty(property, object))
+console.log(hasProperty("location", {"location": {"longitude": 1.2345, "latitude": 5.4321}}))
+
