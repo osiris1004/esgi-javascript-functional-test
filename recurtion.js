05fe0363b1750ce6f4b7d9596465edcb8d02057b
osiris <kontchou05@gmail.com>
2021-12-17 01:04:10 +0100

    new v

--- recurtion.js
+++ recurtion.js
@@ -1,33 +1,13 @@
-/*const object = ["jay","max","jack"]
-
- function recurtion(object, n){
-     if(n<0){
-         return n
-     }else{
-         console.log(object[n])
-       
-        return recurtion(object, n-1)
-
-
-     }
- }
-
- console.log(recurtion(object,3))
-hasProperty("location", {"location": {"longitude": 1.2345, "latitude": 5.4321}})
-
- //******************************** */
-
- hasProperty("location", {"location": {"longitude": 1.2345, "latitude": 5.4321}})
- var replaceKeysInObj = (property,object) => {
-    const newObj = {};
-    for (const key in obj){
-      if(ogKey === key) {
-        newObj[newKey] = obj[ogKey]
-   } else if (typeof obj[ogKey] === 'object') {
-     newObj[ogKey] = replaceKeysInObj(obj[ogKey], key, newKey);
-   } else {
-     newObj[ogKey] = obj[ogKey]
-   }
- }
- return newObj
+ const flattenObject = (object) => {
+  const flattened = {}
+  for(prop in object) {
+      if(typeof object[prop] === "object") {
+          flattened = [flattened, ...flattenObject(object[prop])]
+      }else{
+          [flattened, ...object[prop]]
+      }
+  }
+  return flattened
 };
+
+flattenObject({"a": 1, "b": {"c": 2, "d": 3}}); // {"a": 1, "b.c": 2, "b.d": 3})
--- test.js
+++ test.js
@@ -1,31 +1,14 @@
 const hasProperty = (property, object)=>{
 
-        if(typeof property === 'string' ){
-            if(typeof object === 'object'){
-              console.log("True for 2 and 3")
-                for(prop in object){
-                  if(property === prop){
-                    return true
-                  }else{
-                        if(typeof object[prop] === 'object'){
-                          return hasProperty(property, object[prop]) 
-                      }else{
-                          return false;
-                      }
-                  }
-              }
-            }else{
-              console.log("test 3")
-              return false;
-            }
-            
+      for(prop in object){
+       
+        if(typeof object[prop]== "object"){
+           console.log(hasProperty(property, object[prop]))
         }else{
-            console.log("test 2")
-            return false;
-        } 
-  
-}
-
+          return false
+        }
+    }   
+}    
 
 console.log(hasProperty("location", {"location": {"longitude": 1.2345, "latitude": 5.4321}}))
 
